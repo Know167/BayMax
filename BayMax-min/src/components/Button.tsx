bf24ffa951b449e62e43bb40aadce44face8908c
Know167 <90709908+Know167@users.noreply.github.com>
2024-10-13 17:52:48 -0400

    updating custom Button for compatibility

--- BayMax-min/src/components/Button.tsx
+++ BayMax-min/src/components/Button.tsx
@@ -3,23 +3,24 @@ import ButtonSvg from "../assets/svg/ButtonSvg";
 type ButtonProps = {
   className: string;
   href?: string;
-  onclick?: React.MouseEventHandler<HTMLButtonElement>;
-  px?: number;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  px?: string;
   white?: boolean;
   children?: React.ReactNode;
 };
 const Button = ({
   className,
   href,
-  onclick,
+  onClick,
   children,
   px,
   white,
-}: ButtonProps):React.ReactNode => {
+}: ButtonProps): React.ReactNode => {
+
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''}`;
   const spanClasses='relative z-10'
     const renderButton = (): React.ReactNode => (
-      <button className={classes} onClick={onclick}>
+      <button className={classes} onClick={onClick}>
         <span className={spanClasses}>{children}</span>
         {ButtonSvg(white!)}
       </button>
